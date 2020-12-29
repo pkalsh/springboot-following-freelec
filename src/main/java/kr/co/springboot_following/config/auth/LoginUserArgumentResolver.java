64f47130c60ce1eaa9dd4c03f884cbdac6ace30f
DongMin-Lee <pkalsh345@gmail.com>
2020-12-30 00:27:11 +0900

    스프링 시큐리티와 Oauth를 이용한 사용자 인증과 권한 구현

---
+++ src/main/java/kr/co/springboot_following/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,38 @@
+package kr.co.springboot_following.config.auth;
+
+import kr.co.springboot_following.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+/*
+ * HandlerMethodArgumentResolver 는 한가지 기능을 지원
+ * 조건에 맞는 경우 메소드가 있다면 HandlerMethodArgumentResolver 의 구현체가 지정한 값으로
+ * 해당 메소드의 파라미터로 넘길 수 있다.
+ */
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("user");
+    }
+}
--- src/main/java/kr/co/springboot_following/web/IndexController.java
+++ src/main/java/kr/co/springboot_following/web/IndexController.java
@@ -1,5 +1,7 @@
 package kr.co.springboot_following.web;
 
+import kr.co.springboot_following.config.auth.LoginUser;
+import kr.co.springboot_following.config.auth.dto.SessionUser;
 import kr.co.springboot_following.service.posts.PostsService;
 import kr.co.springboot_following.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,18 +10,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         // 서버 템플릿 엔진에서 사용할 수 있는 객체를 Model 에 저장한다.
         // index.mustache 에 전달한다.
 
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
