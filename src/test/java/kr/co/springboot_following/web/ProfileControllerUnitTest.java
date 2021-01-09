6ae2e94a3fad49443001e91049af11e5c38efe2c
DongMin-Lee <pkalsh345@gmail.com>
2021-01-09 18:19:12 +0900

    무중단 배포를 위한 프로파일 코드 작성

---
+++ src/test/java/kr/co/springboot_following/web/ProfileControllerUnitTest.java
@@ -0,0 +1,59 @@
+package kr.co.springboot_following.web;
+
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerUnitTest {
+
+    @Test
+    public void showRealProfile() {
+        // given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        // when
+        String profile = controller.profile();
+
+        // then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void showFirstWhenNotHaveRealProfile() {
+        // given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        // when
+        String profile = controller.profile();
+
+        // then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void showDefaultWhenNotHaveActiveProfile() {
+        // given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        // when
+        String profile = controller.profile();
+
+        // then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
