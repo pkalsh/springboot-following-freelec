b8f700f5e50647f8d965069457ca088ccec3b11f
DongMin-Lee <pkalsh345@gmail.com>
2021-01-09 17:37:50 +0900

    배포 스크립트 오타 수정2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ PROJECT_NAME=springboot-following-freelac
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/.CDN/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
